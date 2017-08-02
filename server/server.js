ddaf20f1ac747489ed152ce4ffaeba31be0acb60
Streed12 <steve.t.reed@gmail.com>
2017-08-02 15:18:20 -0700

    Need to start filling out answers. Basic server functionality works.

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/cards/:cardType', (req, res) => {
+  const { cardType } = req.params
+  let dirJSON = path.join(__dirname + `/../cards/${cardType}Answers.json`);
+  let ourData = fs.readFileSync(dirJSON)
+  ourData = JSON.stringify(JSON.parse(ourData), null, 2)
+  res.send(ourData)
+})
+
+app.get('*', (req, res) => {
+  res.status(500).send('error: no such endpoint')
+})
+
+app.listen(port, (err) => {
+  if(err) return console.log(err)
+  console.log('listening on port', port)
+})
---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import logo from './logo.svg';
+import './App.css';
+
+const clueAPI = 'http://jservice.io/api/clues?value=1000&offset=';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      offset: 25,
+      questionPool: [],
+      isLoading: true,
+      curr: 0,
+      displayAnswer: false,
+    }
+    this.getFlashCards = this.getFlashCards.bind(this)
+    this.getNextCard = this.getNextCard.bind(this)
+    this.showAnswer = this.showAnswer.bind(this)
+  }
+  componentWillMount() {
+  
+  }
+  async getFlashCards() {
+    let { offset } = this.state
+    try {
+      let {data: [ [...questions] ]} = await axios.get('http://localhost:3000/cards/html')
+      this.setState({
+        questionPool: questions,
+        isLoading: false,
+        curr: 0
+      })
+    } catch(e){
+      console.log('ERROR', e)
+    }
+  }
+  getNextCard () {
+    let i = this.state.curr + 1;
+    if(i === 98){
+      this.getFlashCards()
+    } else {
+      this.setState({curr: i, displayAnswer: false})
+    }
+  }
+
+  showAnswer(e) {
+    e.preventDefault();
+    this.setState({
+      displayAnswer: true
+    })
+  }
+  render() {
+    let questionCard = (<div></div>)
+    if(!this.state.isLoading){
+    const { questionPool, curr } = this.state
+    console.log(questionPool[curr])
+    const { id, question, answer } = questionPool[curr];
+    questionCard = (
+      <div>
+        <ul>
+          <li>{id}</li>
+          <li>{question}</li>
+          <li>{this.state.displayAnswer ? answer : ''}</li>
+        </ul>
+      </div>
+      )
+    }
+
+    const buttonState = this.state.isLoading ? <button onClick={this.getFlashCards}>Get Cards</button> : <div><button onClick={this.getNextCard}>Get Next Card</button> <button onClick={this.showAnswer}>See Answer</button></div>
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro"></p>
+          <div>
+            {questionCard}
+          </div>
+
+        {buttonState}
+      </div>
+    );
+  }
+}
+
+export default App;
